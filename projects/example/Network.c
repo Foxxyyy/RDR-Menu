469527b0f48380b2776b09296a6f6f35fb9f3ae6
Im_Foxxyyy <antoinebdu85@gmail.com>
2022-04-15 22:57:30 +0200

    Update Network.c

--- projects/example/Network.c
+++ projects/example/Network.c
@@ -18,16 +18,7 @@ void OnlinePlayerSubMenu(void* param)
 void TeleportToPlayer(void* param)
 {
 	int index = *(int*)param;
-	vector3 position;
-
-	if (!IS_SLOT_VALID(selectedPlayerId))
-	{
-		printf("Failed !\n<red>Invalid player</red>");
-		return;
-	}
-	GET_SLOT_POSITION(selectedPlayerId, &position);
-	TeleportActor(MySelf, position);
-	printf("Teleported to %s", GetSlotColor(selectedPlayerId));
+	//Do stuff
 }
 
 void NetLoopedFunctions()
