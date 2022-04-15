a181cb4f6a3c71ed1f3d7deec06c51ad2c3e04d1
Im_Foxxyyy <antoinebdu85@gmail.com>
2022-04-15 22:18:15 +0200

    Update ItemExecution.h

--- projects/example/ItemExecution.h
+++ projects/example/ItemExecution.h
@@ -4,6 +4,8 @@
 #pragma GCC diagnostic ignored "-Wextern-initializer"
 
 //Declarations
+void SetCreditsFunction(void* param);
+void SubMenu1(void* param);
 void NetworkOptions(void* param);
 void MainMenu();
 void LoopedOptions();
